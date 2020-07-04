7d615dc0bcea465cfc079da87fa77d84a8b677a9
Robin Reicher <robin.o.reicher@gmail.com>
2020-07-04 13:11:44 +0200

    split up tests

---
+++ unittest/test_riksdagen.py
@@ -0,0 +1,23 @@
+import unittest
+
+import riksdagen
+
+
+class test_riksdagen(unittest.TestCase):
+    def test_extract(self):
+        assert False
+
+    def test_get(self):
+        assert False
+
+    def test_get_ledamoten(self):
+        assert False
+
+    def test_get_voteringar(self):
+        assert False
+
+    def test_get_anforande(self):
+        assert False
+
+if __name__ == '__main__':
+    unittest.main()
